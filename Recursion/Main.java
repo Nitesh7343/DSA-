e7a64a3fe5d2c7d6af4756aad2853ced5c6a790a
Nitesh <niteshsingh7343@gmail.com>
2025-12-28 03:17:15 +0530

    Custom Linked List

--- Recursion/Main.java
+++ Recursion/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
     public static int factorial(int n) {
         if(n == 0 || n == 1) return 1;
-        return n*factorial(n-1);
+        return n * factorial(n - 1);
+
+
     }
 }
---
+++ Recursion/SortArray.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class SortArray {
+    public static void main(String[] args) {
+        int[] arr = {1,5,9,3,4,2,6,2,8,9,1,0,2,32,0};
+
+    }
+}
